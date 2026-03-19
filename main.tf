58a5255e57e75ee200b03b5e2af1cbfb040d5f78
lbajwa <laks.bajwa@googlemail.com>
2026-03-19 16:23:29 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+variable "gcp_project" {}
+
+provider "google" {
+  project = var.gcp_project 
+  region  = "us-west1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-remote-state-student_04_7e3e893ce338-12974-17243"
+    prefix  = "terraform/state/lab01" 
+  }
+}
+
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true 
